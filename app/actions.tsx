6c8250db6d4b4ce872d90a811476df215102124b
Eric Ross <epr@rossmbox.com>
2025-08-25 19:46:13 -0700

    working on form

---
+++ app/actions.tsx
@@ -0,0 +1,10 @@
+'use server'
+
+
+export async function createPost(formData:FormData) {
+    const title = formData.get('title')
+    const content = formData.get('content')
+
+    console.log(title);
+    console.log(content);
+}
---
+++ app/formdemo/page.tsx
@@ -0,0 +1,7 @@
+
+import { createPost } from '@/app/actions';
+
+export default function page() {
+    return (
+      <p>formdata is here</p>
+    )}
